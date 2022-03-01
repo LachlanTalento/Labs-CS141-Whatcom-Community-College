62b387c0b687290babd77cd7c661b8aeef843e38
PorkeyYT <62969602+PorkeyYT@users.noreply.github.com>
2022-03-01 12:23:59 -0800

    Update Instructions.java

--- Instructions.java
+++ Instructions.java
@@ -1,5 +1,12 @@
 public class Instructions {
    public static void main(String[] args) {
-      println
+      System.out.println("This program will run a guessing game.");
+      System.out.println("I will think of a number between 1 and");
+      System.out.println("100 and I will allow you to guess until");
+      System.out.println("you get it right. For each guess, I will");
+      System.out.println("tell you whether the right answer is");
+      System.out.println("higher or lower than your previous");
+      System.out.println("guess.");
+      System.out.println();
    }
 }
